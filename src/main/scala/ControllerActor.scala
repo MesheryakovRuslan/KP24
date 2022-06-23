f2f61246e5dd2b0c31ef6bc8a6377b37fceca5f3
Мещеряков Степан <kosmo.zlo@yandex.ru>
2022-06-23 14:55:08 +0400

    KP24

--- src/main/scala/ControllerActor.scala
+++ src/main/scala/ControllerActor.scala
@@ -1,16 +1,29 @@
-
+import akka.actor.TypedActor.context
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, Behavior}
 
 object ControllerActor {
-  final case class Greet(whom: String, replyTo: ActorRef[Greeted])
-  final case class Greeted(whom: String, from: ActorRef[Greet])
+//  trait RoomCommand
+//  final case class getSession (userName : String, replyTo:ActorRef[ChatEvent]) extends RoomCommand
 
-  def apply(): Behavior[Greet] = Behaviors.receive { (context, message) =>
-    context.log.info("Hello {}!", message.whom)
-    message.replyTo ! Greeted(message.whom, context.self)
-    Behaviors.same
-  }
+  trait ChatEvent
+  final case class sendMessage (textMessage:String,recipientName:String) extends ChatEvent
+  final case class receiveMessages (textMessage:String,senderName:String) extends ChatEvent
+//  final case class friendRequest (friendName:String) extends ChatEvent
+//  final case class replyToFriendRequest (friendName:String, reply: Boolean) extends ChatEvent
+//  final case class sendMessageToConversation ()
+//  final case class receiveMessageToConversation ()
 
+  def apply():Behavior[ChatEvent]
+  Behaviors.setup(context => new ControllerActorBehavior(context))
+
+  def controllerActor(sessions: List[ActorRef[ChatEvent]]):Behavior[ChatEvent] =
+    Behaviors.receive{(context,message) =>
+      message match {
+        case sendMessage(textMessage,recipientName) =>
+
+
+      }
+    }
   }
 
--- src/main/scala/HelloWorldBot.scala
+++ src/main/scala/HelloWorldBot.scala
@@ -1,22 +1,2 @@
-import akka.actor.typed.Behavior
-import akka.actor.typed.scaladsl.{Behaviors, LoggerOps}
 
-object HelloWorldBot {
-
-    def apply(max: Int): Behavior[ControllerActor.Greeted] = {
-      bot(0, max)
-    }
-
-    private def bot(greetingCounter: Int, max: Int): Behavior[ControllerActor.Greeted] =
-      Behaviors.receive { (context, message) =>
-        val n = greetingCounter + 1
-        context.log.info2("Greeting {} for {}", n, message.whom)
-        if (n == max) {
-          Behaviors.stopped
-        } else {
-          message.from ! ControllerActor.Greet(message.whom, context.self)
-          bot(n, max)
-        }
-      }
-  }
 
