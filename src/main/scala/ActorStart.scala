bbdc3137f8042974b1a2c19e8dcc8bebb8876645
Ruslan <meduzaruslan@gmail.com>
2022-07-20 00:09:10 +0400

    KP24

--- src/main/scala/ActorStart.scala
+++ src/main/scala/ActorStart.scala
@@ -10,14 +10,22 @@ class ActorStart(controller: ChatMainControllerLogic, app: Main, ip: String, por
       controller.actorSystem = ActorSystem(ActorMain(controller), "AkkaController", getConfigHost())
       app.actorSystem = controller.actorSystem
     }else{
-      controller.actorSystem = ActorSystem(ActorMain(controller), "AkkaController", getConfigConnect(nodePort,nodeIP))
-      app.actorSystem = controller.actorSystem
+      var search: Boolean = true
+        var portConnect:Int = 2550
+        do {
+          try {
+            controller.actorSystem = ActorSystem(ActorMain(controller), "AkkaController", getConfigConnect(portConnect,ip))
+            search = false
+            println(ip+":"+port)
+          }
+          catch {
+            case _: Throwable => portConnect += 1
+          }
+        }
+        while (search && portConnect < portConnect + 10)
     }
 
-
-
-
-  def getConfigConnect(port: String, ip: String): Config = {
+  def getConfigConnect(port: Int, ip: String): Config = {
     ConfigFactory.parseString(
       s"""
       akka.cluster.seed-nodes = ["akka://AkkaController@$ip:$port"]
@@ -30,7 +38,7 @@ class ActorStart(controller: ChatMainControllerLogic, app: Main, ip: String, por
     ConfigFactory.parseString(
       s"""
       akka.remote.artery.canonical.hostname = "0.0.0.0"
-      akka.remote.artery.canonical.port = $port
+      akka.remote.artery.canonical.port = 2551
     """).withFallback(ConfigFactory.load())
   }
 }
